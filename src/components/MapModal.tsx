f49a7ae6401b6cfd2a6a4acb38a886f3e5c762cd
PowerDayz <Jonas@sellmer.dk>
2023-09-18 16:15:45 +0200

    Update MapModal.tsx

--- src/components/MapModal.tsx
+++ src/components/MapModal.tsx
@@ -5,7 +5,7 @@ import GtaMap from './../mapassets/gtav-map-atlas-huge.jpg';
 import PlayerMarker from './../mapassets/playermarker.png';
 
 function MapModal({ open, handleClose, position, darkMode }: { open: boolean, handleClose: () => void, position: { x: number, y: number, z: number }, darkMode: boolean }) {
-  const mapRef = useRef(null);
+  const mapRef = useRef<HTMLDivElement | null>(null);
   let map: L.Map;
   let marker: L.Marker;
 
@@ -19,37 +19,45 @@ function MapModal({ open, handleClose, position, darkMode }: { open: boolean, ha
   const YOffsetCoord = 4350;
 
   useEffect(() => {
-    if (open && mapRef.current) {
-      map = L.map(mapRef.current, {
-        crs: L.CRS.Simple,
-        minZoom: -15,
-        center: [1500, 1500],
-      }).setView([position.y-YOffsetCoord, position.x], 1);
+    if (open) {
+      const initializeMap = () => {
+        if (mapRef.current) {
+          map = L.map(mapRef.current, {
+            crs: L.CRS.Simple,
+            minZoom: -1.5,
+            center: [1500, 1500],
+          }).setView([position.y-YOffsetCoord, position.x], -1.5);
 
-      const imageUrl = GtaMap;
-      const xScale = (6690.999 + 5659.689) / 2000;
-      const yScale = (8426.220 + 4058.537) / 2000;
+          const imageUrl = GtaMap;
+          const xScale = (6690.999 + 5659.689) / 2000;
+          const yScale = (8426.220 + 4058.537) / 2000;
 
-      const leftBound = 0 * xScale - 5659.689;
-      const topBound = 0 * yScale - 8426.220;
-      const rightBound = 2000 * xScale - 5659.689;
-      const bottomBound = 2000 * yScale - 8426.220;
+          const leftBound = 0 * xScale - 5659.689;
+          const topBound = 0 * yScale - 8426.220;
+          const rightBound = 2000 * xScale - 5659.689;
+          const bottomBound = 2000 * yScale - 8426.220;
 
-      const imageBounds: L.LatLngBoundsExpression = [
-        [topBound, leftBound] as L.LatLngTuple, 
-        [bottomBound, rightBound] as L.LatLngTuple
-      ];      
+          const imageBounds: L.LatLngBoundsExpression = [
+            [topBound, leftBound] as L.LatLngTuple, 
+            [bottomBound, rightBound] as L.LatLngTuple
+          ];      
 
-      L.imageOverlay(imageUrl, imageBounds).addTo(map);
-      marker = L.marker([position.y-YOffsetCoord, position.x], {icon: markerIcon}).addTo(map);
+          L.imageOverlay(imageUrl, imageBounds).addTo(map);
+          marker = L.marker([position.y-YOffsetCoord, position.x], {icon: markerIcon}).addTo(map);
+        } else {
+          setTimeout(initializeMap, 100);
+        }
+      };
+
+      initializeMap();
     }
 
     return () => {
-      if (map) {
-        map.remove();
-      }
+        if (map) {
+            map.remove();
+        }
     };
-  }, [open, position]);
+  }, [open]);
 
   return (
     <Modal
@@ -69,7 +77,7 @@ function MapModal({ open, handleClose, position, darkMode }: { open: boolean, ha
         }}
       >
         <Typography id="map-modal-title" variant="h5" style={{ marginBottom: 20/* , userSelect: 'none' */ }}>Player Coords: {position.x}, {position.y}, {position.z}</Typography>
-        <div id="map" ref={mapRef} style={{ height: 650, width: '100%' }}></div>
+        {open && <div id="map" ref={mapRef} style={{ height: 650, width: '100%' }}></div>}
       </Box>
     </Modal>
   );
